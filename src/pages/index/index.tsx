0757361ccad588611ba37fb27e386e96b3958add
黄晨 <18136225579@163.com>
2019-03-26 02:41:45 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,70 +1,13 @@
 import { ComponentClass } from 'react';
 import Taro, { Component, Config } from '@tarojs/taro';
-import { View, Button, Text } from '@tarojs/components';
-import { connect } from '@tarojs/redux';
+import { View } from '@tarojs/components';
 import BarChart from '../../components/BarChart';
 import PieChart from '../../components/PieChart';
 import LineChart from '../../components/LineChart';
-import { add, minus, asyncAdd } from '../../actions/counter';
 
 import './index.less';
 
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
-
-type PageStateProps = {
-  counter: {
-    num: number;
-  };
-};
-
-type PageDispatchProps = {
-  add: () => void;
-  dec: () => void;
-  asyncAdd: () => any;
-};
-
-type PageOwnProps = {};
-
-type PageState = {};
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps;
-
-interface Index {
-  props: IProps;
-}
-
-@connect(
-  ({ counter }) => ({
-    counter
-  }),
-  dispatch => ({
-    add() {
-      dispatch(add());
-    },
-    dec() {
-      dispatch(minus());
-    },
-    asyncAdd() {
-      dispatch(asyncAdd());
-    }
-  })
-)
 class Index extends Component {
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
     navigationBarTitleText: '首页'
   };
@@ -121,15 +64,16 @@ class Index extends Component {
     this.lineChart.refresh(chartDataLine);
   }
 
-  refBarChart = node => (this.barChart = node);
+  refBarChart = (node: any) => (this.barChart = node);
 
-  refPieChart = node => (this.pieChart = node);
+  refPieChart = (node: any) => (this.pieChart = node);
 
-  refLineChart = node => (this.lineChart = node);
+  refLineChart = (node: any) => (this.lineChart = node);
 
   render() {
     return (
-      <View>
+      <View className='wrap'>
+        <View className='br'></View>
         <View className='bar-chart'>
           <BarChart ref={this.refBarChart} />
         </View>
@@ -144,11 +88,4 @@ class Index extends Component {
   }
 }
 
-// #region 导出注意
-//
-// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
-// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
-//
-// #endregion
-
-export default Index as ComponentClass<PageOwnProps, PageState>;
+export default Index as ComponentClass;
